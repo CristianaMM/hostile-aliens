d99b32bbf06d2bf64da7afe970af0d34ec4f3aee
Cristiana Monteiro <cristianamelomonteiro@gmail.com>
2022-01-26 19:40:48 +0000

    Game logic

--- scripts/game/game.js
+++ scripts/game/game.js
@@ -3,20 +3,25 @@ class Ship {
     this.id = id;
     this.points = 0;
     this.damage = 0;
+    this.element = document.querySelector(`#${id}`);
+    this.elementPoints = document.querySelector(`#${id}>p`);
   }
 
-  getId() {
-    return this.id;
-  }
-
-  getPoints() {
-    return this.points;
+  updatePoints() {
+    this.elementPoints.innerHTML = `${this.points}`;
   }
 
   hit() {
     this.points -= this.damage;
+    this.updatePoints();
+  }
 
-    return this.points;
+  destroy() {
+    this.element.style.visibility = "hidden";
+  }
+
+  display() {
+    this.element.style.visibility = "visible";
   }
 }
 
@@ -44,14 +49,57 @@ class AttackShip extends Ship {
   }
 }
 
+const fireButton = document.querySelector(".player");
+const restartButton = document.querySelector(".gameOverModal__button");
+const gameOverModal = document.querySelector(".gameOverModal");
 let ships = [];
 
-for (let i = 1; i <= 14; i++) {
-  if (i === 1) {
-    ships.push(new MotherShip(`ship${i}`));
-  } else if (i > 1 && i <= 6) {
-    ships.push(new DefenceShip(`ship${i}`));
-  } else {
-    ships.push(new AttackShip(`ship${i}`));
+function setUpGame() {
+  for (let i = 0; i < 14; i++) {
+    if (i === 0) {
+      ships.push(new MotherShip(`ship_${i}`));
+    } else if (i > 0 && i <= 5) {
+      ships.push(new DefenceShip(`ship_${i}`));
+    } else {
+      ships.push(new AttackShip(`ship_${i}`));
+    }
+  }
+
+  for (const ship of ships) {
+    ship.updatePoints();
+    ship.display();
   }
 }
+
+function gameOver() {
+  for (const ship of ships) {
+    ship.destroy();
+    ships = [];
+  }
+
+  gameOverModal.style.display = "flex";
+}
+
+function fire() {
+  const index = Math.floor(Math.random() * ships.length);
+  const ship = ships[index];
+
+  ship.hit();
+
+  if (ship.points <= 0) {
+    ships.splice(index, 1);
+    ship.destroy();
+
+    if (index === 0) {
+      gameOver();
+    }
+  }
+}
+
+fireButton.addEventListener("click", () => fire());
+restartButton.addEventListener("click", () => {
+  setUpGame();
+  gameOverModal.style.display = "none";
+});
+
+setUpGame();
